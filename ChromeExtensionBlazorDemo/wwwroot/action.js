30227dfc02e9596526d0c01cd7ed66a56175e847
Ernest Scott Skousen <ernest@division4studios.com>
2023-05-26 11:05:10 -0400

    Working?

---
+++ ChromeExtensionBlazorDemo/wwwroot/action.js
@@ -0,0 +1,4 @@
+﻿// Import for the side effect of defining a global 'browser' variable
+chrome.browserAction.onClicked.addListener(function () {
+    chrome.tabs.create({ url: chrome.runtime.getURL("index.html") });
+});
